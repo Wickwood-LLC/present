580a7ec9ee446296174dccf0f90f427b0b19a0a5
Junaid PV <junu.pv@gmail.com>
2025-02-12 21:37:53 +0530

    Rename to Bacground Audio.

--- src/Plugin/RevealJSPlugin/BackgroundAudio.php
+++ src/Plugin/RevealJSPlugin/BackgroundAudio.php
@@ -5,8 +5,8 @@ namespace Drupal\present\Plugin\RevealJSPlugin;
 use Drupal\present\Attribute\RevealJSPlugin;
 use Drupal\present\Plugin\RevealJSPlugin\RevealJSPluginInterface;
 
-#[RevealJSPlugin("full_audio")]
-class FullAudio implements RevealJSPluginInterface {
+#[RevealJSPlugin("background_audio")]
+class BackgroundAudio implements RevealJSPluginInterface {
 
   /**
    * {@inheritdoc}
