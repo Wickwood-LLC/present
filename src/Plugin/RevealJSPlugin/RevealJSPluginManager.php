0e1af14b0224cbb6f78cb47a38fb82ae1a6e5000
Junaid PV <junu.pv@gmail.com>
2025-02-12 22:09:23 +0530

    Correct docs.

--- src/Plugin/RevealJSPlugin/RevealJSPluginManager.php
+++ src/Plugin/RevealJSPlugin/RevealJSPluginManager.php
@@ -8,20 +8,16 @@ use Drupal\Core\Plugin\DefaultPluginManager;
 use Drupal\present\Attribute\RevealJSPlugin;
 
 /**
- * Provides a plugin manager for element plugins.
+ * Provides a plugin manager for RevealJS plugins.
  *
- * @see \Drupal\Core\Render\Attribute\RenderElement
- * @see \Drupal\Core\Render\Attribute\FormElement
- * @see \Drupal\Core\Render\Element\RenderElementBase
- * @see \Drupal\Core\Render\Element\FormElementBase
- * @see \Drupal\Core\Render\Element\ElementInterface
- * @see \Drupal\Core\Render\Element\FormElementInterface
+ * @see \Drupal\present\Attribute\RevealJSPlugin
+ * @see \Drupal\present\Plugin\RevealJSPlugin\RevealJSPlugin
  * @see plugin_api
  */
 class RevealJSPluginManager extends DefaultPluginManager {
 
   /**
-   * Constructs an ElementInfoManager object.
+   * Constructs an RevealJSPluginManager object.
    *
    * @param \Traversable $namespaces
    *   An object that implements \Traversable which contains the root paths
@@ -32,8 +28,6 @@ class RevealJSPluginManager extends DefaultPluginManager {
    *   The module handler to invoke the alter hook with.
    */
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
-    // $this->setCacheBackend($cache_backend, 'element_info');
-
     parent::__construct('Plugin/RevealJSPlugin', $namespaces, $module_handler, 'Drupal\present\Plugin\RevealJSPlugin\RevealJSPluginInterface', RevealJSPlugin::class);
     $this->alterInfo('revealjs_plugin');
     $this->setCacheBackend($cache_backend, 'revealjs_plugin_info');
