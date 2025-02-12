fe359821c51589cda3690c71a024cdc5d614f6e4
Junaid PV <junu.pv@gmail.com>
2025-02-12 21:46:55 +0530

    Set ID and label for the Background Audio plugin.

--- src/Plugin/RevealJSPlugin/BackgroundAudio.php
+++ src/Plugin/RevealJSPlugin/BackgroundAudio.php
@@ -2,10 +2,14 @@
 
 namespace Drupal\present\Plugin\RevealJSPlugin;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\present\Attribute\RevealJSPlugin;
 use Drupal\present\Plugin\RevealJSPlugin\RevealJSPluginInterface;
 
-#[RevealJSPlugin("background_audio")]
+#[RevealJSPlugin(
+  id: 'background_audio',
+  label: new TranslatableMarkup('Background Auido'),
+)]
 class BackgroundAudio implements RevealJSPluginInterface {
 
   /**
