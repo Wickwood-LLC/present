9f1e518bfea5d16b2428360a71350b49bf33369e
Junaid PV <junu.pv@gmail.com>
2025-02-13 14:23:20 +0530

    Remove auto playing as it will be blocked by browsers by default. Support buttons in slide content with special data attribute and the audio will start once clicked on it.

--- js/revealjs.bg-audio.js
+++ js/revealjs.bg-audio.js
@@ -1,26 +1,31 @@
 window.BackgroundAudio = window.BackgroundAudio || {
     id: 'BackgroundAudio',
+    playing: false,
     init: function(deck) {
-        initBackgroundAudio(deck);
-    }
-};
+        let reveal_element = deck.getRevealElement();
+        let plugin = this;
+        // Get all start buttons
+        const bg_start_buttons = reveal_element.querySelectorAll('[data-bg-audio-start-button]');
 
-const initBackgroundAudio = function(deck){
-    
-    deck.addEventListener('ready', function( event ) {
+        // Loop through the buttons and attach click event listener
+        bg_start_buttons.forEach(child => {
+            child.addEventListener('click', function (event){
+                plugin.startAudio(deck);
+            });
+        });
+    },
+    startAudio: function(deck) {
         let config = deck.getConfig();
         if ('background_audio' in config) {
             let audio = new Audio(config.background_audio);
             if (audio) {
                 deck.configure({autoSlide: 5000, autoSlideStoppable: false, controls: false})
+                audio.play();
             }
             // audio.addEventListener("timeupdate", (event) => {
             //     // console.log(audio.currentTime);
             // });
-            audio.play();
         }
-		// selectAudio();
-		// document.dispatchEvent( new CustomEvent('stopplayback') );
-	} );
+    }
 
-}
+};
