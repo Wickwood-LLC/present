06498535d0fe604c7a7532daf8bcf25ed897bd8b
Junaid PV <junu.pv@gmail.com>
2025-02-14 15:56:33 +0530

    Improve docmentation comment.

--- src/Attribute/RevealJSPlugin.php
+++ src/Attribute/RevealJSPlugin.php
@@ -10,14 +10,14 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 /**
  * Defines a RevealJS plugin attribute object.
  *
- * Plugin Namespace: Plugin\RevealJS\Plugin
+ * Plugin Namespace: Plugin\RevealJSPlugin
  *
- * For a working example, see \Drupal\present\Plugin\RevealJS\Plugin\FullAudio.
+ * For a working example, see \Drupal\present\Plugin\RevealJSPlugin\BackgroundAudio.
  *
- * @see \Drupal\Core\Render\ElementInfoManager
- * @see \Drupal\Core\Render\Element\ElementInterface
- * @see \Drupal\Core\Render\Element\RenderElementBase
- * @see \Drupal\Core\Render\Attribute\FormElement
+ * @see \Drupal\present\Plugin\RevealJSPlugin\RevealJSPluginManager
+ * @see \Drupal\present\Plugin\RevealJSPlugin\RevealJSPluginInterface
+ * @see \Drupal\present\Plugin\RevealJSPlugin\RevealJSPluginBase
+ * @see \Drupal\present\Plugin\RevealJSPlugin\ConfigurableRevealJSPluginBase
  * @see plugin_api
  *
  * @ingroup theme_render
